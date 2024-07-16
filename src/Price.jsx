e57693c78e99ca0edd98393fbf18572210d8205e
Hi, I'm Balikis Omolewa <106351278+Balikis-omolewa@users.noreply.github.com>
2024-07-16 11:55:36 +0100

    Add files via upload
    initial commit

---
+++ src/Price.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Price = ({ price }) => {
+  return <h3>Price: {price}</h3>;
+};
+
+export default Price;
---
+++ src/product.jsx
@@ -0,0 +1,22 @@
+const products = [
+  {
+    name: "2014 MERCEDES-BENZ ML 350 ML 350",
+    price: "$5,300.00",
+    description: "This is a sample product description for the first product.",
+    image: "https://img.freepik.com/free-psd/modern-car-isolated_23-2151504562.jpg?t=st=1720885137~exp=1720888737~hmac=d0e4c0628c95d3387dd915b9a64e6ff0bf38f358ce66c52257e49fd60154afa0&w=740"
+  },
+  {
+    name: "2020 BMW X5 xDrive40i",
+    price: "$60,000.00",
+    description: "This is a sample product description for the second product.",
+    image: "https://img.freepik.com/free-photo/view-3d-car_23-2150796894.jpg?t=st=1720891058~exp=1720894658~hmac=f1814c89809b0ebeb9af4341f9b8a9a2d31515ed84178f4b843470ecbd383d87&w=740"
+  },
+  {
+    name: "2019 AUDI Q7 55 Premium Plus",
+    price: "$55,000.00",
+    description: "This is a sample product description for the third product.",
+    image: "https://img.freepik.com/free-photo/3d-car-with-simple-background_23-2150796878.jpg?t=st=1720891927~exp=1720895527~hmac=e5471c983755aa2379ce8c179a25eb8869f8a40b8e0e35725cf7a9364b43b291&w=740"
+  }
+];
+
+export default products;
